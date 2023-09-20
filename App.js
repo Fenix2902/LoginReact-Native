52375b4b68bd3ab97300b876bd8bb7019a45d4e9
Danna Lorena Salinas Sánchez <143885505+JeronimoVM@users.noreply.github.com>
2023-09-20 07:42:46 -0500

    AM 20/09/2023 Se inicia proyecto de Login, configurando componente de login, y estilos

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import LoginScreen from "./components/LoginScreen";
+import { styles } from "./assets/styles/allstyles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* // <Text>Open up App.js to start working on your app!</Text> */}
+      <LoginScreen />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
